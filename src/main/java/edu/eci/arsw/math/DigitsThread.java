067d5df65ecbbffbce622fc928e218b6909e1cff
unknown <karol.rodriguez-r@labinfo.is.escuelaing.edu.co>
2026-09-04 12:38:01 -0500

    feature: initial second point

--- src/main/java/edu/eci/arsw/math/DigitsThread.java
+++ src/main/java/edu/eci/arsw/math/DigitsThread.java
@@ -44,7 +44,7 @@ public class DigitsThread extends Thread {
             sum = 16 * (sum - Math.floor(sum));
             digits[i] = (byte) sum;
         }
-
+        System.out.println(digits);
         return digits;
     }
 
